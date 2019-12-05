fc0504deb2132617caf00566774b8e90250fd763
Ken Chow <kchow.312@gmail.com>
2019-12-05 15:22:55 +0000

    conflict resolved

--- backend/router.js
+++ backend/router.js
@@ -1,9 +1,6 @@
 const router = require('express').Router()
-<<<<<<< HEAD
 const locations = require('./controllers/locations')
-=======
 const users = require('./controllers/users')
->>>>>>> development
 
 router.route('/locations')
   .get(locations.index)
@@ -12,8 +9,6 @@ router.route('/locations')
 router.route('/locations/:id')
   .put(locations.update)
   .delete(locations.remove)
-
-
 
 router.route('/register')
   .post(users.register)
