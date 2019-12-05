6cd81ae639af0af63ff7a198cadecc6c92ce4b9a
Jonny Garius <gariusjonathan@hotmail.co.uk>
2019-12-05 15:02:44 +0000

    added functionality

--- backend/controllers/locations.js
+++ backend/controllers/locations.js
@@ -14,7 +14,37 @@ function indexPublic(req, res) {
     .catch(err => console.log(err))
 }
 
+function indexCircle(req, res) {
+  Location
+    .find({ privacy: { $eq: 2 } })
+    .then(locations => res.status(200).json(locations))
+    .catch(err => console.log(err))
+}
+
+function indexPrivate(req, res) {
+  Location
+    .find({ privacy: { $eq: 3 } })
+    .then(locations => res.status(200).json(locations))
+    .catch(err => console.log(err))
+}
+
+function show(req, res) {
+  Location
+    .findById(req.params.id)
+    .then(location => {
+      if (!location) {
+        return res.status(404).json({ message: '404 not found' })
+      } else {
+        return res.status(200).json(location)
+      }
+    })
+    .catch(err => console.log(err))
+}
+
 module.exports = {
   index,
-  indexPublic
+  indexPublic,
+  indexCircle,
+  indexPrivate,
+  show
 }
--- backend/router.js
+++ backend/router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
+const locations = require('./controllers/locations')
 
 router.route('/locations')
-  .get((req, res) => res.status(200).json({ message: 'yasss' } ))
+  .get(locations.index)
+
+router.route('/locations/public')
+  .get(locations.indexPublic)
+
+router.route('/locations/circle')
+  .get(locations.indexCircle)
+
+router.route('/locations/private')
+  .get(locations.indexPrivate)
+
+router.route('/locations/:id')
+  .get(locations.show)
 
 module.exports = router
 
