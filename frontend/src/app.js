34de7d5398283e319cb0b9cfaee819b68f02d4a9
Jonny Garius <gariusjonathan@hotmail.co.uk>
2019-12-06 16:52:04 +0000

    added map

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import ReactMap from 'react-map-gl'
 
 import 'bulma'
 import './style.scss'
@@ -12,10 +13,66 @@ const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Home} />
+      <Route exact path='/map' component={Map} />
     </Switch>
   </BrowserRouter>
 )
 
+class Map extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      viewport: {
+        width: '50vw',
+        height: '50vh',
+        latitude: 51.51491,
+        longitude: -0.07280,
+        zoom: 16
+      },
+      locations: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/locations/available', {
+      headers: { Authorization: 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZGVhNzRkODgzOGQ4N2MxZmU0ZDcwMmMiLCJpYXQiOjE1NzU2NDY0MjcsImV4cCI6MTU3NTczMjgyN30.y-CL4Z_6XuECIRWX7HjuYaiEjw8jzs9n6DDI4Y5wm9c' }
+    })
+      .then(resp => {
+        const locations2 = []
+        const locations1 = resp.data
+        const promise = new Promise(function (resolve, reject) {
+          locations1.forEach(location => {
+            return (
+              axios.get(`https://api.postcodes.io/postcodes/${location.postcode}`)
+                .then(resp => {
+                  console.log(resp.data.result.latitude, resp.data.result.longitude)
+                  location.latitude = resp.data.result.latitude
+                  location.longitude = resp.data.result.longitude
+                  locations2.push(location)
+                  resolve(locations2)
+                })
+                // .catch(err => reject(err))
+            )
+          })
+        })
+        return promise
+      })
+      .then(resp => this.setState({ locations: resp }))
+    
+  }
+
+  render() {
+    console.log(this.state.locations)
+    return <div className="section">
+      <ReactMap
+        mapboxApiAccessToken="pk.eyJ1IjoiamdhciIsImEiOiJjazNicmRob2MwOTM0M2R1aW9iMjJpdHBxIn0.b-gHKxL-hNP7YOODnakv7Q"
+        { ...this.state.viewport }
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+      </ReactMap>
+    </div>
+  }
+}
 
 
 
